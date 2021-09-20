da352e76486fe5e7c0799bd3ef62e16ac2995568
Kseniya Kuzmina <kseniya.kuzmina@devexpress.com>
2021-09-20 10:40:11 +0300

    update example

--- CS/Window1.xaml.cs
+++ CS/Window1.xaml.cs
@@ -1,10 +1,37 @@
-﻿using System.Windows;
-
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
 namespace Point2DChart {
-
-    public partial class Window1 : Window {
-        public Window1() {
-            InitializeComponent();
+    public class ChartViewModel {
+        List<DataPoint> dataPoints;
+        public List<DataPoint> DataPoints {
+            get {
+                if (dataPoints == null) {
+                    dataPoints = new List<DataPoint> {
+                            new DataPoint (new DateTime(2019, 1, 1), 7.2),
+                            new DataPoint (new DateTime(2019, 2, 1), 8.3),
+                            new DataPoint (new DateTime(2019, 3, 1), 12.2),
+                            new DataPoint (new DateTime(2019, 4, 1), 15.6),
+                            new DataPoint (new DateTime(2019, 5, 1), 19.6),
+                            new DataPoint (new DateTime(2019, 6, 1), 22.7),
+                            new DataPoint (new DateTime(2019, 7, 1), 25.2),
+                            new DataPoint (new DateTime(2019, 8, 1), 25),
+                            new DataPoint (new DateTime(2019, 9, 1), 21.1),
+                            new DataPoint (new DateTime(2019, 10, 1), 16.3),
+                            new DataPoint (new DateTime(2019, 11, 1), 10.8),
+                            new DataPoint (new DateTime(2019, 12, 1), 7.5)
+                };
+                }
+                return dataPoints;
+            }
+        }
+    }
+    public class DataPoint {
+        public DateTime Time { get; set; }
+        public double Value { get; set; }
+        public DataPoint(DateTime time, double value) {
+            this.Time = time;
+            this.Value = value;
         }
     }
 }
--- Readme.md
+++ Readme.md
@@ -3,16 +3,23 @@
 [![](https://img.shields.io/badge/Open_in_DevExpress_Support_Center-FF7200?style=flat-square&logo=DevExpress&logoColor=white)](https://supportcenter.devexpress.com/ticket/details/E1651)
 [![](https://img.shields.io/badge/📖_How_to_use_DevExpress_Examples-e9f6fc?style=flat-square)](https://docs.devexpress.com/GeneralInformation/403183)
 <!-- default badges end -->
-<!-- default file list -->
-*Files to look at*:
 
-* **[Window1.xaml](./CS/Window1.xaml) (VB: [Window1.xaml](./VB/Window1.xaml))**
-<!-- default file list end -->
 # How to create a 2D Point chart
 
+This example shows how to create a [2D Point](https://docs.devexpress.com/WPF/5868/controls-and-libraries/charts-suite/chart-control/series/2d-series-types/point-line-and-bubble-series/point) chart and configure its titles, label format, axes, and appearance.
 
-<p>The following example demonstrates how to create a 2D Point chart.</p>
+![](Images/2D-Point-chart-example.png)
 
-<br/>
+<!-- default file list -->
+## Files to Look At
 
+- [Window1.xaml](./CS/Window1.xaml) (VB: [Window1.xaml](./VB/Window1.xaml))
+- [Window1.xaml.cs](./CS/Window1.xaml.cs) (VB: [Window1.xaml.vb](./VB/Window1.xaml.vb))
 
+<!-- default file list end -->
+
+## Documentation
+
+- [How to: Create a 2D Point Chart](https://docs.devexpress.com/WPF/6866/controls-and-libraries/charts-suite/chart-control/examples/2d-chart-types/how-to-create-a-2d-point-chart)
+- [Series](https://docs.devexpress.com/WPF/6339/controls-and-libraries/charts-suite/chart-control/series/series)
+- [2D Series Types](https://docs.devexpress.com/WPF/114223/controls-and-libraries/charts-suite/chart-control/series/2d-series-types)
