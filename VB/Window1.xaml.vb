62606063a046751df01a2bdabbed6f46bd5da86f
DevExpressExampleBot <fakeEmail>
2021-09-20 11:05:53 +0300

    Source auto update [skip ci]

---
+++ VB/Window1.xaml.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Namespace Point2DChart
+	Public Class ChartViewModel
+'INSTANT VB NOTE: The field dataPoints was renamed since Visual Basic does not allow fields to have the same name as other class members:
+		Private dataPoints_Conflict As List(Of DataPoint)
+		Public ReadOnly Property DataPoints() As List(Of DataPoint)
+			Get
+				If dataPoints_Conflict Is Nothing Then
+					dataPoints_Conflict = New List(Of DataPoint) From {
+						New DataPoint(New DateTime(2019, 1, 1), 7.2),
+						New DataPoint(New DateTime(2019, 2, 1), 8.3),
+						New DataPoint(New DateTime(2019, 3, 1), 12.2),
+						New DataPoint(New DateTime(2019, 4, 1), 15.6),
+						New DataPoint(New DateTime(2019, 5, 1), 19.6),
+						New DataPoint(New DateTime(2019, 6, 1), 22.7),
+						New DataPoint(New DateTime(2019, 7, 1), 25.2),
+						New DataPoint(New DateTime(2019, 8, 1), 25),
+						New DataPoint(New DateTime(2019, 9, 1), 21.1),
+						New DataPoint(New DateTime(2019, 10, 1), 16.3),
+						New DataPoint(New DateTime(2019, 11, 1), 10.8),
+						New DataPoint(New DateTime(2019, 12, 1), 7.5)
+					}
+				End If
+				Return dataPoints_Conflict
+			End Get
+		End Property
+	End Class
+	Public Class DataPoint
+		Public Property Time() As DateTime
+		Public Property Value() As Double
+		Public Sub New(ByVal time As DateTime, ByVal value As Double)
+			Me.Time = time
+			Me.Value = value
+		End Sub
+	End Class
+End Namespace
